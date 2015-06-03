7166cbcadd480fd3ec3158b12a850aa830e1094a
eshuster <eshuster3@gmail.com>
2015-06-03 13:50:11 -0700

    Add Ajax search call instead of Ruby

--- Rise_n_Pine/app/assets/javascripts/articles_index.js
+++ Rise_n_Pine/app/assets/javascripts/articles_index.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+  var ul = $('<ul></ul>')
+
   var response = $.ajax({
     url: 'http://localhost:3000/articles_index/',
     type: "GET"
@@ -11,25 +13,43 @@ $(document).ready(function(){
       var category = data[count].category;
       if (category === "Music") {
         var musicHeader = $('<div class="articles-music-header"></div>')
-        var href = $('<a class="article-index-link"  href="http://localhost:3000/articles/' + data[count].id  + '/edit"' + ' ' + '>Edit</a>');
+        var Header = $('<div></div>')
+        var a_href = $('<a class="article-edit-link"  href="http://localhost:3000/articles/' + data[count].id  + '/edit"' + ' ' + '>Edit</a>');
+        var s_href = $('<a class="article-edit-link" href="http://localhost:3000/articles/' + data[count].id + '">Show</a>');
+        var d_href = $('<a class="article-edit-link" href="http://localhost:3000/articles/' + data[count].id + '/destroy">Delete</a>');
+
         musicHeader.append("&nbsp;" + data[count].id + ". ");
         musicHeader.append("&nbsp;" + data[count].title);
         musicHeader.append("&nbsp;" + data[count].author);
         musicHeader.append("&nbsp;" + data[count].date);
-        musicHeader.append(href[0]);
+
+        Header.append(s_href[0]);
+        Header.append(a_href[0]);
+        Header.append(d_href[0]);
+        musicHeader.append(Header)
+
         $('.main-cont-left').append(musicHeader)
-        count++;
+          count++;
       } else {
         var newsHeader = $('<div class="articles-news-header"></div>');
-        var href = $('<a class="article-index-link"  href="http://localhost:3000/articles/' + data[count].id  + '/edit"' + ' ' + '>Edit</a>');
+        var Header = $('<div></div>');
+        var a_href = $('<a class="article-edit-link"  href="http://localhost:3000/articles/' + data[count].id  + '/edit"' + ' ' + '>Edit</a>');
+        var s_href = $('<a class="article-edit-link" href="http://localhost:3000/articles/' + data[count].id + '">Show</a>');
+        var d_href = $('<a class="article-edit-link" href="http://localhost:3000/articles/' + data[count].id + '/destroy">Delete</a>');
+
         newsHeader.append("&nbsp;" + data[count].id + ". ");
         newsHeader.append("&nbsp;" + data[count].title);
         newsHeader.append("&nbsp;" + data[count].author);
         newsHeader.append("&nbsp;" + data[count].date);;
-        newsHeader.append(href[0]);
+
+        Header.append(s_href[0]);
+        Header.append(a_href[0]);
+        Header.append(d_href[0]);
+        newsHeader.append(Header)
+
         $('.main-cont-right').append(newsHeader);
-        count++;
+          count++;
       };
-    }
+    };
   });
 });
--- Rise_n_Pine/config/routes.rb
+++ Rise_n_Pine/config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # get 'user/index'
 
   root 'users#index'
   get '/login' => 'sessions#new'
@@ -8,30 +7,23 @@ Rails.application.routes.draw do
 
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
-  # get '/search-artist' => 'articles#searchartist'
-  get '/articles_index' => 'articles#articles_index'
 
   get '/searchartist/:artist' => 'articles#searchartist'
 
-  # get '/articles_show' => 'articles#articles_show'
+  get '/articles_index' => 'articles#articles_index'
+  get '/articles_search/:title' => 'articles#articles_search'
+
+
 
   resources :articles do
     # resources :tags do
     member do
-      delete 'destroy'
+      get 'destroy'
       post 'destroy'
       put 'update'
       patch 'update'
     # end
+    end
   end
-end
-
-  # resources :tags do
-  #   member do
-  #   end
-  # end
-
-
-
 
 end
